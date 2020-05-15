1a67a10c4bbc2a9af41c0823273a81f4b20e5844
spectralcodex <fr3man.stephen@gmail.com>
2020-05-15 19:10:01 +0000

    Jdbc Implementations added

--- microservice-easyerp-common/src/main/java/io/vertx/easyerp/common/service/JdbcRepositoryWrapper.java
+++ microservice-easyerp-common/src/main/java/io/vertx/easyerp/common/service/JdbcRepositoryWrapper.java
@@ -1,11 +1,13 @@
 package io.vertx.easyerp.common.service;
 
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
 
 /**
  * Helper and wrapper class for JDBC repository services ported from origional blueprint
@@ -21,12 +23,43 @@ public class JdbcRepositoryWrapper {
         this.client = JDBCClient.createNonShared(vertx, config);
     }
 
-    protected  void executeNoResult(JsonArray params, String sql, Handler<AsyncResult<Void>> resultHandler){
+    /**
+     * @param params
+     * @param sql
+     * @param resultHandler
+     */
+    protected void executeNoResult(JsonArray params, String sql, Handler<AsyncResult<Void>> resultHandler) {
+        client.getConnection(connHandler(resultHandler, connection -> {
+            connection.updateWithParams(sql, params, r -> {
+                if (r.succeeded()) {
+                    resultHandler.handle(Future.succeededFuture());
+                } else {
+                    resultHandler.handle(Future.failedFuture(r.cause()));
+                }
+                connection.close();
+            });
+        }));
+    }
+
+    protected <R> void execute(JsonArray params, String sql, R ret, Handler<AsyncResult<R>> resultHandler) {
 
     }
 
-    protected  <R> void execute(JsonArray params, String sql, R ret, Handler<AsyncResult<R>> resultHandler){
+    /**
+     * A helper methods that generates async handler for SQLConnection
+     *
+     * @return generated handler
+     */
+    protected <R> Handler<AsyncResult<SQLConnection>> connHandler(Handler<AsyncResult<R>> h1, Handler<SQLConnection> h2) {
+        return conn -> {
+            if (conn.succeeded()) {
+                final SQLConnection connection = conn.result();
+                h2.handle(connection);
+            } else {
+                h1.handle(Future.failedFuture(conn.cause()));
+            }
+        };
 
     }
-
 }
+
